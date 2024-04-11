e6f4ec063fb8f973893569671b1687051f7dc1cb
ahmedjilani123 <ahmedjilani0011@gmail.com>
2024-04-11 12:22:47 +0530

    add vercel

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
-app.get('/', function(req, res){
-res.send("welcome")
+app.get('/ahmed', function(req, res){
+res.send("welcome ahmed")
 });
 app.listen(3001,()=>{
     console.log('listening on');
