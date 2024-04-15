29fb3be24f27305750d044a24aa266f7a0eb086d
ahmedjilani123 <ahmedjilani0011@gmail.com>
2024-04-15 12:37:30 +0530

    add employees

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
 const express = require('express');
 const app = express();
-app.get('/ahmed', function(req, res){
-res.send("welcome ahmed")
+const jsonM ={
+    "data":[
+      {
+      "user":"Zaid",
+      "age":20,
+      "gender":"Male"
+      },
+       {
+      "user":"Dev",
+      "age":20,
+      "gender":"Male"
+      },
+       {
+      "user":"joshi sir",
+      "age":26,
+      "gender":"Male"
+      }
+    ]
+  }
+app.get('/daffodils', function(req, res){
+res.json(jsonM)
 });
 app.listen(3001,()=>{
     console.log('listening on');
