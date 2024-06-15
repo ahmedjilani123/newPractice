837aec12ea7b9d6ec9169d4538e6bf37dc54ae16
ahmedjilani123 <ahmedjilani0011@gmail.com>
2024-06-15 11:06:26 +0530

    all is set

---
+++ UserSubmit.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+const UserSubmit = new Schema({
+    Name:{
+        type:String
+    },
+    Email:{
+        type:String
+    },
+    Country:{
+        type:String
+    },
+    Telephone:{
+        type:String
+    }
+})
+module.exports =mongoose.model("UserSubmit",UserSubmit);
--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const cors=require("cors");
 const mongoose = require('mongoose');
 mongoose.connect(process.env.DATABASE)
 const Data = require("./Schema");
+const UserSubmit =require("./UserSubmit");
 app.use(cors({
   "origin":"*"
 }));
@@ -13,16 +14,26 @@ const port=process.env.PORT || 3000;
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
-app.get('/daffodils', async function(req, res){
+app.get('/vendorsub', async function(req, res){
   var data = await Data.find();
 res.json(data);
 });
-app.post('/daffodil',async function(req, res){
+app.post('/vendorsub',async function(req, res){
     var Userdata = req.body;
-    console.log(Userdata);
+    Userdata.Status="Vendor Submitted";
     await Data.create(Userdata);
     res.json({"Message":"Created"});
     });
+  app.get('/UserSubmit', async function(req, res){
+      var data = await UserSubmit.find();
+    res.status(201).json(data);
+    });
+    app.post('/UserSubmit',async function(req, res){
+        var Userdata = req.body;
+         await UserSubmit.deleteMany();
+        await UserSubmit.create(Userdata);
+        res.status(201).json({"Message":"Submit"});
+        });
 app.listen(port,()=>{
     console.log('listening on');
 })
