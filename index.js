4dcf1918869fa3557e9586c14f61b8ebcbcedf2e
ahmedjilani123 <ahmedjilani0011@gmail.com>
2024-06-18 10:22:43 +0530

    set space

--- index.js
+++ index.js
@@ -3,24 +3,33 @@ const app = express();
 const env = require("dotenv").config();
 const cors=require("cors");
 const mongoose = require('mongoose');
+
 const { vendorsub, vendorpost } = require('./controller/vendorsub');
 const { SubmitGet, submitPost } = require('./controller/usersubmit');
 const { UserStatus } = require('./controller/UserStatus');
+
+
 mongoose.connect(process.env.DATABASE)
 app.use(cors({
   "origin":"*"
 }));
+
 const port=process.env.PORT || 3000;
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
+
+
 app.get('/vendorsub', vendorsub);
 app.post('/vendorsub',vendorpost);
-
 app.get('/UserSubmit',SubmitGet);
 app.post('/UserSubmit',submitPost);
 app.post("/userven",UserStatus)
+
+
+
+
 app.listen(port,()=>{
     console.log('listening on');
 })
